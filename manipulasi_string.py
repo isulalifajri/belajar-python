e7dff9f76d2ffe4818435bedbb9f6ede89d9643e
isulalifajri@gmail.com <isulalifajri@gmail.com>
2025-10-28 09:38:23 +0700

    manipulasi string

---
+++ manipulasi_string.py
@@ -0,0 +1,8 @@
+nama = "langit"
+umur = 26
+
+pesan = "nama saya " + nama + ", umur " + str(umur)
+
+print(pesan)
+print(len(nama))
+print(len(pesan))
--- variabel.py
+++ variabel.py
@@ -5,3 +5,5 @@ tinggi = 160
 print(nama)
 print(umur)
 print(tinggi)
+
+print("hello", nama, "umur", umur, "tinggi", tinggi)
