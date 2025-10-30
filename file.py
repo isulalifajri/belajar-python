d14ee924c167545a2c175403ede54bf2594a5c0e
isulalifajri@gmail.com <isulalifajri@gmail.com>
2025-10-30 17:01:16 +0700

    menampilkan isi file

--- file.py
+++ file.py
@@ -1,17 +1,30 @@
-print("SIMPAN DATA NILAI")
+# print("SIMPAN DATA NILAI")
 
-file = open("nilai_siswa.txt", "w")
+# file = open("nilai_siswa.txt", "w")
 
-while True:
-    nama = input("Nama siswa (enter utk selesai): ")
-    if nama == "":
-        break
+# while True:
+#     nama = input("Nama siswa (enter utk selesai): ")
+#     if nama == "":
+#         break
 
-    nilai = input("Nilai: ")
+#     nilai = input("Nilai: ")
 
-    # Tulis ke file
-    file.write(nama + "," + nilai + "\n")
-    print("Data", nama, "berhasil disimpan")
+#     # Tulis ke file
+#     file.write(nama + "," + nilai + "\n")
+#     print("Data", nama, "berhasil disimpan")
+
+# file.close()
+# print("Semua data berhasil disimpan ke nilai_siswa.txt")
+
+
+print("MENAMPILKAN DATA NILAI")
+
+file = open("nilai_siswa.txt", "r")
+
+for line in file:
+    data = line.strip().split(",")
+    print(data[0], ":", data[1])
 
 file.close()
-print("Semua data berhasil disimpan ke nilai_siswa.txt")
+
+print("SELESAI")
