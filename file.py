2b7721e20f8de6809f54dcf9891025dda5d83c0d
isulalifajri@gmail.com <isulalifajri@gmail.com>
2025-10-30 17:09:04 +0700

    menggunakan try catch untuk penanganan error

--- file.py
+++ file.py
@@ -27,9 +27,12 @@ print("MENAMPILKAN DATA NILAI")
 
 # file.close()
 
-with open("nilai_siswa.txt", "r") as file:
-    for line in file:
-        data = line.strip().split(",")
-        print(data[0], ":", data[1])
+try:
+    with open("nilai_siswa.txt", "r") as file:
+        for line in file:
+            data = line.strip().split(",")
+            print(data[0], ":", data[1])
+except FileNotFoundError:
+    print("file tidak ditemukan")
 
 print("SELESAI")
